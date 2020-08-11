92e8c8c7741e20980f811bb3b26453aca35609bd
Divit SANJAY KARMIANI <divit.sanjaykarmiani@amadeus.com>
2020-08-11 21:58:50 +0530

    Updates

---
+++ precache-manifest.c46934651065501291d87c882542739d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241628a2d628dfe8cf7eba462837b90d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aa39ab984cf2de405cc1",
+    "url": "/portfolio/static/css/main.b3e89040.chunk.css"
+  },
+  {
+    "revision": "4c0ac53a720c3620778f",
+    "url": "/portfolio/static/js/2.6a9d513d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.6a9d513d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa39ab984cf2de405cc1",
+    "url": "/portfolio/static/js/main.4dbb1e90.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
