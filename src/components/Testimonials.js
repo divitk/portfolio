8eb5db97e1aaad45930c0255976d09505b21dfce
unknown <divitkarmiani1998@gmail.com>
2021-08-06 20:33:03 +0530

    Added Testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,14 +1,13 @@
 
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import mine from '../images/mine.jpg';
 import quoteicon from '../images/quoteicon.png';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Jude from '../images/jude.jpg';
-
+import Mohit from '../images/mohit.jpg';
 
 
 import '../css/Testimonials.css';
@@ -47,7 +46,7 @@ function Testimonials() {
 <Container className='testimonial' style={{width:'450px'}}>
     <Row >
        <Col xs={3}  className='text-right' style={{padding:'0px'}}>
-       <Image className='img-fluid' src={mine} alt="Avatar"  roundedCircle  style={{width:'55px', height:'55px'}} />
+       <Image className='img-fluid' src={Mohit} alt="Avatar"  roundedCircle  style={{width:'55px', height:'55px'}} />
            </Col>
       <Col xs={9}  className='text-left px-3'  >
       <p style={{padding:'3px 0px 0px 0px', margin:'0px'}}>Mohit Kumar</p>
